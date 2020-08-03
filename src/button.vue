900d942572db20202336f399893fd92ab9fa9fcc
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-03 21:02:57 +0800

    给 Button 添加了 icon 和 iconPosition 属性 分别负责 icon 图标的种类，控制左右位置

--- src/button.vue
+++ src/button.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>
-        <button class="k-button">按钮1</button>
-    </div>
+    <button class="k-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+
 </template>
 
 <script>
-    export default {};
+    export default {
+        props: ['icon', 'iconPosition']
+    };
 </script>
 
 <style scoped lang="scss">
-    .k-button{
+
+    .k-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
@@ -18,15 +27,44 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        vertical-align: middle;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
 
-        &:hover{
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
+
+        &:active {
             background-color: var(--button-active-bg);
         }
-        &:focus{
+
+        &:focus {
             outline: none;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+            width: 1em;
+            height: 1em;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-left: .3em;
+                margin-right: 0;
+            }
+
+            > .content {
+                order: 1;
+            }
         }
     }
 
