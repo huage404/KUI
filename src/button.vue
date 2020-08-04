fa788795d40eb8bea2214d0b656ba894fa674bfd
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-04 11:40:06 +0800

    通过 validator 验证 iconPosition 中值之内为 left 或 right

--- src/button.vue
+++ src/button.vue
@@ -12,7 +12,16 @@
 
 <script>
     export default {
-        props: ['icon', 'iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator: function (value) {
+                    return !(value !== 'left' && value !== 'right');
+                }
+            }
+        }
     };
 </script>
 
