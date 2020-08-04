d58f4c9e023cb59104fd4b0cd0422d798fa49af1
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-04 16:29:29 +0800

    新增了 button-group.vue 组件，用于包裹多个 k-button

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@
 import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
+import ButtonGroup from './button-group';
 
 Vue.component('k-button' , Button);
 Vue.component('k-icon' , Icon);
+Vue.component('k-button-group' , ButtonGroup);
 
 
 new Vue({
---
+++ src/button-group.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="k-button-group">
+        <slot></slot>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            for( let node of this.$el.children ){
+                let name = node.nodeName.toLowerCase();
+                if(node.nodeName.toLowerCase() !== 'button'){
+                    console.warn(`k-button-group 的直接子元素应该全是 k-button 但你写的是 ${name}`)
+                }
+            }
+        }
+    }
+
+</script>
+
+<style scoped lang="scss">
+    .k-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+
+        > .k-button{
+            -webkit-border-radius: 0;
+            -moz-border-radius: 0;
+            border-radius: 0;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+            
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
