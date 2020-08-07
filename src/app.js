e135534a7f85424f5ad322402c892fdca958ab5f
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-07 10:12:59 +0800

    基本实现 row 和 col 的功能

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
-
 import Vue from 'vue';
 import Button from './button';
 import Icon from './icon';
 import ButtonGroup from './button-group';
 import Input from './input';
+import Row from './row';
+import Col from './col';
 
-Vue.component('k-button' , Button);
-Vue.component('k-icon' , Icon);
-Vue.component('k-button-group' , ButtonGroup);
-Vue.component('k-input' , Input);
+Vue.component('k-button', Button);
+Vue.component('k-icon', Icon);
+Vue.component('k-button-group', ButtonGroup);
+Vue.component('k-input', Input);
+Vue.component('k-row', Row);
+Vue.component('k-col', Col);
 
 
 new Vue({
-    el:"#app",
-    data:{
+    el: "#app",
+    data: {
         loading1: false,
         loading2: false,
         loading3: false,
         value: ''
     },
-    methods:{
-        inputChange(e){
+    methods: {
+        inputChange(e) {
             console.log('Hello World');
             console.log(e.target.value);
             console.log(e.target._value);
