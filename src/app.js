8188e170e4359acdb2ca9096a5593119230e74ea
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-08 17:24:54 +0800

    添加了布局功能，组件分别为：layout\header\content\footer\sider , 并为其添加了一些简单的动画效果

--- src/app.js
+++ src/app.js
@@ -6,12 +6,24 @@ import Input from './input';
 import Row from './row';
 import Col from './col';
 
+import Layout from './layout'
+import Header from './header'
+import Sider from './sider'
+import Content from './content'
+import Footer from './footer'
+
 Vue.component('k-button', Button);
 Vue.component('k-icon', Icon);
 Vue.component('k-button-group', ButtonGroup);
 Vue.component('k-input', Input);
 Vue.component('k-row', Row);
 Vue.component('k-col', Col);
+
+Vue.component('k-layout', Layout)
+Vue.component('k-header', Header)
+Vue.component('k-content', Content)
+Vue.component('k-footer', Footer)
+Vue.component('k-sider', Sider)
 
 
 new Vue({
---
+++ src/footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="footer">
+        <slot></slot>
+    </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+    .footer {
+    }
+</style>
