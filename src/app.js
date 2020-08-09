4d020c82b5b22b70e5ade8638ddc72abe4d1be10
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-09 16:22:09 +0800

    实现 toast 组件，用于弹出信息，可以设置出现位置 tpp/center/bottom

--- src/app.js
+++ src/app.js
@@ -1,46 +1,67 @@
-import Vue from 'vue';
-import Button from './button';
-import Icon from './icon';
-import ButtonGroup from './button-group';
-import Input from './input';
-import Row from './row';
-import Col from './col';
-
+import Vue from 'vue'
+import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
+import Input from './input'
+import Row from './row'
+import Col from './col'
 import Layout from './layout'
 import Header from './header'
 import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
 
-Vue.component('k-button', Button);
-Vue.component('k-icon', Icon);
-Vue.component('k-button-group', ButtonGroup);
-Vue.component('k-input', Input);
-Vue.component('k-row', Row);
-Vue.component('k-col', Col);
-
+Vue.component('k-button', Button)
+Vue.component('k-icon', Icon)
+Vue.component('k-button-group', ButtonGroup)
+Vue.component('k-input', Input)
+Vue.component('k-row', Row)
+Vue.component('k-col', Col)
 Vue.component('k-layout', Layout)
 Vue.component('k-header', Header)
 Vue.component('k-content', Content)
 Vue.component('k-footer', Footer)
 Vue.component('k-sider', Sider)
+Vue.component('k-toast', Toast)
+Vue.use(plugin)
+
 
 
 new Vue({
-    el: "#app",
+    el: '#app',
     data: {
         loading1: false,
-        loading2: false,
+        loading2: true,
         loading3: false,
-        value: ''
+        message: 'hi'
+    },
+    created(){
     },
     methods: {
-        inputChange(e) {
-            console.log('Hello World');
-            console.log(e.target.value);
-            console.log(e.target._value);
+        showToast1(){
+            this.showToast('top')
+        },
+        showToast2(){
+            this.showToast('middle')
+        },
+        showToast3(){
+            this.showToast('bottom')
+        },
+        showToast(position){
+            this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+                position,
+                enableHtml: false,
+                closeButton: {
+                    text: '已充值',
+                    callback () {
+                        console.log('他说已经充值智商了')
+                    }
+                },
+                autoClose: 3,
+            })
         }
     }
-});
-
+})
 
