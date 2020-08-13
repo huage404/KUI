cc1fa3e4fd10eb0a8d31aced295a846e58270efe
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-13 17:32:14 +0800

    popover 功能基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div ref="contentWrapper" class="content-wrapper" v-show="visible" @click.stop>
+    <div class="popover" @click="onClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-show="visible">
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
@@ -12,48 +12,63 @@
 <script>
     export default {
         name: "k-popover",
-        data(){
-            return{
+        data() {
+            return {
                 visible: false
             }
         },
-        methods:{
-            xxx(){
-                this.visible = !this.visible;
-                if(this.visible === true){
-                    setTimeout(()=>{
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {left,top} = this.$refs.triggerWrapper.getBoundingClientRect();
-                        this.$refs.contentWrapper.style.left = left+'px';
-                        this.$refs.contentWrapper.style.top = top +'px';
-                        let eventHandler = ()=>{
-                            this.visible = false;
-                            document.removeEventListener('click',eventHandler);
-                        }
-                        document.addEventListener('click',eventHandler);
-                    });
+        methods: {
+            positionContent() {  // 让展示层定位到按钮附近
+                document.body.appendChild(this.$refs.contentWrapper);
+                let {left, top} = this.$refs.triggerWrapper.getBoundingClientRect();
+                this.$refs.contentWrapper.style.left = window.scrollX + left + 'px';
+                this.$refs.contentWrapper.style.top = window.scrollY + top + 'px';
+            },
+            onClickDocument(e) {
+                if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+                    return
                 }
+                this.close();
+            },
+            open() {  // 点击后触发的逻辑
+                this.visible = true;
+                setTimeout(() => {
+                    this.positionContent();
+                    document.addEventListener('click', this.onClickDocument);
+                });
+            },
+            close: function () {
+                this.visible = false;
+                document.removeEventListener('click',this.onClickDocument);
+            },
+            onClick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
+                    if (this.visible === true) {
+                        this.close();
+                    } else {
+                        this.open();
+                    }
+                }
+
             }
-        },
-        mounted() {
-            console.log(this.$refs.triggerWrapper)
         }
     }
 </script>
 
 <style scoped lang="scss">
-    .popover{
+    .popover {
         display: inline-block;
         vertical-align: top;
         position: relative;
 
     }
-    .content-wrapper{
+
+    .content-wrapper {
         position: absolute;
         border: 1px solid red;
-        -webkit-box-shadow: 0 0 3px rgba(0,0,0,.5);
-        -moz-box-shadow: 0 0 3px rgba(0,0,0,.5);
-        box-shadow: 0 0 3px rgba(0,0,0,.5);
+        -webkit-box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+        -moz-box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+        box-shadow: 0 0 3px rgba(0, 0, 0, .5);
         -webkit-transform: translateY(-100%);
         -moz-transform: translateY(-100%);
         -ms-transform: translateY(-100%);
