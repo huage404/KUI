1287f2a0febec65eb1d197727cf718941b2a9cb7
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-08-14 14:55:33 +0800

    collapse 添加 single 属性

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="collapseItem" @click="open = !open">
+    <div class="collapseItem" @click="toggle">
         <div class="title">
             {{title}}
         </div>
@@ -12,25 +12,46 @@
 <script>
     export default {
         name: "k-collapse-item",
-        props:{
+        props: {
             title: {
                 type: String,
                 required: true
             }
         },
-        data(){
-            return{
+        data() {
+            return {
                 open: false
             }
-        }
+        },
+        inject: ['eventBus'],
+        mounted() {
+            this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+                if (vm !== this) {
+                    this.close();
+                }
+            })
+        },
+        methods: {
+            toggle() {
+                if (this.open) {
+                    this.open = false;
+                } else {
+                    this.open = true;
+                    this.eventBus && this.eventBus.$emit('update:selected', this)
+                }
+            },
+            close() {
+                this.open = false;
+            }
+        },
     }
 </script>
 
 <style scoped lang="scss">
     $grey: #ddd;
     $border-radius: 4px;
-    .collapseItem{
-        >.title{
+    .collapseItem {
+        > .title {
             border: 1px solid $grey;
             margin: -1px -1px 0;
             min-height: 32px;
@@ -39,22 +60,20 @@
             padding: 0 8px;
         }
 
-        > .content{
+        > .content {
             padding: 8px;
         }
 
 
-
-
-
-        &:first-child{
-            > .title{
+        &:first-child {
+            > .title {
                 border-top-left-radius: $border-radius;
                 border-top-right-radius: $border-radius;
             }
         }
-        &:last-child{
-            > .title:last-child{
+
+        &:last-child {
+            > .title:last-child {
                 border-bottom-left-radius: $border-radius;
                 border-bottom-right-radius: $border-radius;
                 border-bottom: none;
--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,28 @@
 </template>
 
 <script>
+    import Vue from 'vue'
     export default {
-        name: "k-collapse"
+        name: "k-collapse",
+        data(){
+            return{
+                eventBus: new Vue()
+            }
+        },
+        props:{
+            single: {
+                type: Boolean,
+                default: false
+            }
+        },
+        provide(){
+            if(this.single){
+                return{
+                    eventBus: this.eventBus
+                }
+            }
+        }
+
     }
 </script>
 
